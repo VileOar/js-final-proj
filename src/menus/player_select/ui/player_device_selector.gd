cb472da8ffdd83a203e18bafb2ddd31c005b15be
PMSValerio <37459155+VileOar@users.noreply.github.com>
2024-11-26 21:02:48 +0000

    added a 'waiting for input' msg to device selector

--- src/menus/player_select/ui/player_device_selector.gd
+++ src/menus/player_select/ui/player_device_selector.gd
@@ -9,12 +9,20 @@ signal toggled_listen(onoff)
 
 ## ref to player identifier
 @onready var _player_label = %PlayerId
-## ref to device identifier
+
+## ref to the group of nodes with the device identification
+@onready var _device_id: HBoxContainer = %DeviceID
+## ref to device identifier text
 @onready var _device_ui = %Device
+## the device icon node
+@onready var _device_icon: Control = %DeviceIcon
+
+## temp text to display when waiting for input
+@onready var _input_msg: Label = %InputMsg
+
 ## ref to the listen button
 @onready var _listen_btn = %ListenButton
 
-@onready var _device_icon: Control = %DeviceIcon
 ## the sprite with the icons
 @onready var _device_sprite: Sprite2D = %DeviceSprite
 @onready var _player_sprite: Sprite2D = %PlayerSprite
@@ -52,13 +60,18 @@ func set_device(device : int):
 				push_warning("added valid device, but not recognised by the UI script???")
 	else:
 		_device_icon.hide()
+		_device_ui.show()
 		_device_ui.text = "CPU"
+	
 
 
 ## cleanup function for when another selector requests listening for a device
 func release_listen():
 	# untoggle the button without sending the signal
 	_listen_btn.set_pressed_no_signal(false)
+	# do this here again just to be sure
+	_device_id.show()
+	_input_msg.hide()
 
 
 ## callback to receive an input event[br]
@@ -79,7 +92,14 @@ func forward_input_event(event : InputEvent):
 
 func _on_device_button_toggled(toggled_on):
 	toggled_listen.emit(toggled_on)
+	if toggled_on:
+		_device_id.hide()
+		_input_msg.show()
+	else:
+		_device_id.show()
+		_input_msg.hide()
 
 
 func _on_ai_button_pressed():
+	toggled_listen.emit(false)
 	set_device(-2)
